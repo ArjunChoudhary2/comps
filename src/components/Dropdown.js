ecffcc8ea2d3ec2b4a9e946631bed050b1486cbc
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2023-08-25 22:49:43 +0530

    Dropdown document click implemented

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { GoArrowDown, GoArrowLeft } from "react-icons/go";
+import Panel from "./Panel";
 
 //UPDATED PROP NAMES TO ONCHANGE FROM ONSELECT AND VALUE FROM OPTIONS
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  const divEl = useRef();
+
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    const handler = (event) => {
+        if(!divEl.current){
+            return;
+        }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
 
   const handleListItemClick = (ListItem) => {
     //CLOSE DROPDOWN
@@ -30,18 +51,15 @@ function Dropdown({ options, value, onChange }) {
 
   //THE EXPERESSION selection?.label GIVES UNDEFINED IF SELECTION.LABEL IS NULL OR UNDEFINED INSTEAD OF AN ERROR
   return (
-    <div className="w-48 relative">
-      <div
-        className="flex justify-between items-center cursor-pointer border-rounded p-3 shadow bg-white w-full"
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
       >
         {value?.label || "Select..."}
-      </div>
-      {isOpen && (
-        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
-          {renderedOption}{" "}
-        </div>
-      )}
+        <GoArrowDown />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOption} </Panel>}
     </div>
   );
 }
