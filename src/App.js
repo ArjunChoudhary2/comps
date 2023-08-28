116a08b04a225988493118a393bc5be39becaec0
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2023-08-28 11:53:42 +0530

    Modal Implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import SideBar from "./components/SideBar";
 import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
-import Route from './components/Route';
+import Route from "./components/Route";
 import ButtonPage from "./pages/ButtonPage";
+import ModalPage from "./pages/ModalPage";
 
 function App() {
-  return <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
-    <SideBar/>
-         <div className="col-span-5">
-            <Route path="/accordion">
-              <AccordionPage/>
-            </Route>
-            <Route path="/">
-              <DropdownPage/>
-            </Route>
-            <Route path="/buttons">
-              <ButtonPage/>
-            </Route>
-          </div>
-  </div>;
+  return (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <SideBar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
+        </Route>
+      </div>
+    </div>
+  );
 }
 
 export default App;
